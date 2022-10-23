fcdcb3ef221156a852a74a62a5062bc3d4ecae9b
ratuurn <114390295+ratuurn@users.noreply.github.com>
2022-10-23 12:20:08 +0700

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,36 @@
+import java.sql.Time;
+
+public class Program {
+
+    public static void main(String[] args) {
+        //Overriding
+
+        Murid mhs = new Murid("Ratu Rismawati");
+        mhs.info();
+
+        mhs = new Mentor("Alun Sujjada M.Kom");
+        mhs.info();
+
+        mhs = new Subject("Pemrograman berorientasi Objek");
+        mhs.info();
+
+        waktu(100);
+        waktu("22 Mei 2022");
+        waktu("Kamis",4, "Sore");
+    }
+
+    //Overloading
+    static void waktu(int Wkt){
+        System.out.println("Waktu Kuliah/M \t : " + Wkt);
+    }
+
+    static void waktu(String Wkt){
+        System.out.println("Pada Tanggal \t : " + Wkt);
+    }
+
+    static void waktu(String hari, int Wkt, String waktu){
+        System.out.println("Waktu \t\t\t : " + hari + ", Pukul " + Wkt + " " + waktu);
+    }
+
+
+}
