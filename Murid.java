c1d2d5cbc11583cf282b476643987f224c4c6dc1
ratuurn <114390295+ratuurn@users.noreply.github.com>
2022-10-23 12:21:29 +0700

    Create Murid.java

---
+++ Murid.java
@@ -0,0 +1,11 @@
+public class Murid {
+    String nama;
+
+    Murid(String nama){
+        this.nama = nama;
+    }
+
+    void info(){
+        System.out.println("Nama Murid \t : " + this.nama);
+    }
+}
